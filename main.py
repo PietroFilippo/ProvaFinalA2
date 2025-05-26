e4292665bd0b6c7c8065494361e26e818d522740
PIETRO FILIPPO FERREIRA E SILVA <pietro.silva@cs.up.edu.br>
2025-05-26 19:19:58 -0400

    Estrutura inicial

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score, f1_score
+import os
+
+def carregar_imagens(diretorio):
+    # TODO
+    pass
+
+def pre_processar_imagem(imagem):
+    # TODO
+    pass
+
+def treinar_model(X_train, y_train):
+    # TODO
+    pass
+
+def avaliar_modelo(modelo, X_test, y_test):
+    # TODO
+    pass
+
+def main():
+    print("Pipeline de processamento de imagens iniciado.")
+    # TODO:
+
+if __name__ == "__main__":
+    main()   
